31ee451f6b62ad38e87036195ee833b1c7fa68d3
raphael.kieling <raphael.kieling-ext@ab-inbev.com>
2025-01-08 10:19:55 -0300

    feat: back to lazygit, way better

---
+++ neovim/.config/nvim/lua/plugins/git.lua
@@ -0,0 +1,43 @@
+return {
+    {
+        "lewis6991/gitsigns.nvim",
+        opts = {
+            signs = {
+                add = { text = "▎" },
+                change = { text = "▎" },
+                delete = { text = "" },
+                topdelete = { text = "" },
+                changedelete = { text = "▎" },
+                untracked = { text = "▎" },
+            },
+            signs_staged = {
+                add = { text = "▎" },
+                change = { text = "▎" },
+                delete = { text = "" },
+                topdelete = { text = "" },
+                changedelete = { text = "▎" },
+            },
+            current_line_blame = true,
+        },
+    },
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>g", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+        },
+    },
+}
--- neovim/.config/nvim/lua/plugins/plugins.lua
+++ neovim/.config/nvim/lua/plugins/plugins.lua
@@ -170,37 +170,6 @@ return {
             },
         },
     },
-    -- Git
-    {
-        "NeogitOrg/neogit",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "sindrets/diffview.nvim",
-            "nvim-telescope/telescope.nvim",
-        },
-        config = true,
-    },
-    {
-        "lewis6991/gitsigns.nvim",
-        opts = {
-            signs = {
-                add = { text = "▎" },
-                change = { text = "▎" },
-                delete = { text = "" },
-                topdelete = { text = "" },
-                changedelete = { text = "▎" },
-                untracked = { text = "▎" },
-            },
-            signs_staged = {
-                add = { text = "▎" },
-                change = { text = "▎" },
-                delete = { text = "" },
-                topdelete = { text = "" },
-                changedelete = { text = "▎" },
-            },
-            current_line_blame = true,
-        },
-    },
     -- Manage the surrounds (parentheses, brackets, etc)
     {
         "kylechui/nvim-surround",
